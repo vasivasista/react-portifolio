eecf9efed3c20c69003dca393d2f77016eba077c
vasivasista <vasistaweapon@gmail.com>
2020-12-26 15:44:16 +0530

    3.Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,50 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How DO I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus sit repudiandae neque adipisci voluptas similique.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus sit repudiandae neque adipisci voluptas similique.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus sit repudiandae neque adipisci voluptas similique.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus sit repudiandae neque adipisci voluptas similique.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default FaqSection;
